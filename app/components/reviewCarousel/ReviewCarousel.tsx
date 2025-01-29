c5775ebb1eff2bbf259ad98b9327feeffeeea4ea
Landon Grinderslev <125698291+lgrinders@users.noreply.github.com>
2025-01-29 14:29:25 -0600

    typescript fixes

--- app/components/reviewCarousel/ReviewCarousel.tsx
+++ app/components/reviewCarousel/ReviewCarousel.tsx
@@ -4,7 +4,7 @@ import { reviewData } from "@/app/assets/data";
 import Autoplay from "embla-carousel-autoplay";
 import useEmblaCarousel from "embla-carousel-react";
 import { useEffect } from "react";
-import { BsFillStarFill } from "react-icons/bs";
+import ReviewCard from "../ui/reviewCard/ReviewCard";
 
 export default function ReviewCarousel() {
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, dragFree: true }, [Autoplay({ delay: 2000 })]);
@@ -36,24 +36,12 @@ export default function ReviewCarousel() {
       <div className="flex">
         {reviewData.map((item, idx) => {
           return (
-            <div
-              className="min-w-[400]  font-semibold p-5 text-md mr-5 h-80 flex flex-col text-neutral-500 justify-evenly rounded-md shadow-xl bg-white select-none"
-              key={idx}
-            >
-              <span className="h-12 text-black flex items-center text-lg">{item.name}</span>
-              <div className="flex h-12 items-center gap-0.5 text-yellow-400">
-                {[...Array(item.stars)].map((_, idx) => {
-                  return <BsFillStarFill size={30} key={idx} />;
-                })}
-              </div>
-              <span className="h-32 flex">
-                {item.body.length > 200 ? `${item.body.substring(0, 200)}...` : item.body}
-              </span>
-              <span className="h-10 flex items-center">{item.date}</span>
-            </div>
+            <ReviewCard key={idx} item={item} />
           );
         })}
       </div>
     </div>
   );
 }
+
+// idx, name, stars, body, date
--- app/components/ui/reviewCard/ReviewCard.tsx
+++ app/components/ui/reviewCard/ReviewCard.tsx
@@ -1,22 +1,33 @@
-import { FaStar } from "react-icons/fa";
+import { BsFillStarFill } from "react-icons/bs";
 
 
-const ReviewCard = ({ id, name, stars, body, date }) => {
-  return (
-    <div
-      className="w-64 h-96 embla__slide bg-white shadow-xl flex flex-col gap-2 rounded-md justify-evenly items-center"
-      key={id}
-    >
-      <span>{name}</span>
-      <div className="flex gap-1">
-        {[...Array(stars)].map((item, idx) => {
-          return <FaStar key={idx} size={25} />;
-        })}
-      </div>
-      <div>{body}</div>
-      <div>{date}</div>
-    </div>
-  );
-};
+interface ReivewCardProps {
 
-export default ReviewCard;
+    item: {
+        name: string;
+        body: string;
+        stars: number;
+        date: string;
+    }
+}
+
+const ReviewCard: React.FC<ReivewCardProps> = ({ item }) => {
+    return (
+        <div
+            className="min-w-[400] font-semibold p-5 text-md mr-5 h-80 flex flex-col text-neutral-500 justify-evenly rounded-md shadow-xl bg-white select-none"
+        >
+            <span className="h-12 text-black flex items-center text-lg">{item.name}</span>
+            <div className="flex h-12 items-center gap-0.5 text-yellow-400">
+                {[...Array(item.stars)].map((_, idx) => {
+                    return <BsFillStarFill size={30} key={idx} />;
+                })}
+            </div>
+            <span className="h-32 flex">
+                {item.body.length > 200 ? `${item.body.substring(0, 200)}...` : item.body}
+            </span>
+            <span className="h-10 flex items-center">{item.date}</span>
+        </div>
+    )
+}
+
+export default ReviewCard
